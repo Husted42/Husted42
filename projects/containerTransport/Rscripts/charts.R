d0f90089f74e5b02bca0855898cfca63a01c6c0f
Husted42 <115221407+Husted42@users.noreply.github.com>
2023-02-04 22:00:06 +0100

    ...

--- projects/containerTransport/Rscripts/charts.R
+++ projects/containerTransport/Rscripts/charts.R
@@ -14,8 +14,10 @@ labels <- c(data$V1)
 percent <- round(100*x/sum(x), 1)
 
 #Plots
-pie(x, labels = percent, main = "Emission class distribution in %", col = rainbow(length(x)))
+pie(x, labels = percent, main = "Enviromental class distribution in %", col = rainbow(length(x)))
 legend("topright", c(data$V1), cex = 0.8,
        fill = rainbow(length(x)))
 
-pie3D(x, labels = labels,explode = 0.1, main = "Emission class distribution ")
+pie3D(x, labels = labels,explode = 0.1, main = "Enviromental class distribution ")
+
+
