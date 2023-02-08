9228bf475525b2e6406d893063d8567da1922b8b
Husted42 <115221407+Husted42@users.noreply.github.com>
2023-02-08 20:41:21 +0100

    ...

--- python/exercise/e2.py
+++ python/exercise/e2.py
@@ -1,15 +1,16 @@
-print("Exercise 2.1-2")
 def exercise_212(A, n):
+    print("Exercise 2.1-2")
     sum = 0
     for i in range(1, n):
         sum = sum + A[i]
     return sum
-lste_212 = [1, 2, 3]
-print(exercise_212(lste_212, len(lste_212)))
 
-print("")
-print("Exercise 2.1-3")
+
+
+
 def exercise_213a(A,n):
+    print("")
+    print("Exercise 2.1-3")
     for i in range (1,n):
         key = A[i]
         j = i - 1
@@ -29,7 +30,12 @@ def exercise_213b(A,n):
         A[j+1] = key
     return A
 
-lste_213 = [5, 2, 4, 6, 1, 3]
-print("ASC : " + str(exercise_213a(lste_213, len(lste_213))))
-print("DESC: " + str(exercise_213b(lste_213, len(lste_213))))
-
+def run ():
+    if (True): # Exercise 2.1-2
+        lste_212 = [1, 2, 3]
+        print(exercise_212(lste_212, len(lste_212)))
+    if (True): # Exercise 2.1-3
+        lste_213 = [5, 2, 4, 6, 1, 3]
+        print("ASC : " + str(exercise_213a(lste_213, len(lste_213))))
+        print("DESC: " + str(exercise_213b(lste_213, len(lste_213))))
+run()
