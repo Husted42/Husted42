10636442ed4aa622f7a6be1b33aa2e6c3595d818
Husted42 <115221407+Husted42@users.noreply.github.com>
2023-02-09 19:14:06 +0100

    jupyter

--- python/exercise/dataSceincePython/assignment1.py
+++ python/exercise/dataSceincePython/assignment1.py
@@ -8,19 +8,31 @@ def read_data (filename):
             values = line.strip().split()
             data.append([float(val) for val in values])
     return data
-
-#print list
-def printRows():
-    list_of_rows = read_data("data.txt")
-    print(list_of_rows)
-
+    
 #Convert a list to a np.array
 def convertToNumpy(lst):
     arr = np.array(lst)
     return arr
 
+#Calculates the average of each column
+def calc_averages(list_of_rows):
+    avg = np.average(list_of_rows, axis=0)
+    return round(avg[0], 4), round(avg[1], 4)
+
+#Changes array of rows into list of columns
+def transpose(input):
+    array = np.array(input).T
+    return array
+
 def run():
-    if (False): printRows()
-    if (True): print(convertToNumpy(read_data("data.txt")))
+    if (False):
+        list_of_rows = read_data("data.txt")
+        print(list_of_rows)
+    if (False): print(convertToNumpy(read_data("data.txt")))
+    if (True):
+        col1_avg, col2_avg = calc_averages(convertToNumpy(read_data("data.txt")))
+        print(col1_avg, col2_avg)
+    if (True):
+        print(transpose(convertToNumpy(read_data("data.txt"))))
 run()
 
