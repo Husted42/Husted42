b3be10af650f8130f23a2b9ce4273ff6a07983f7
Husted42 <115221407+Husted42@users.noreply.github.com>
2023-02-09 13:11:33 +0100

    ...

---
+++ python/test.py
@@ -0,0 +1,18 @@
+# def exercise (A, B, n):
+#     for 1 in n:
+#         b[i] = A[1]
+#         for j in i:
+#             return b
+
+A = [1, 2, 3, 4]
+B = [1, 2, 4, 6]
+
+def exercise (A, B, n):
+    i = 0
+    
+    while i <= n:
+        some = some + 1
+    return some
+
+print(exercise(10))
+#print(exercise(A, B, 10))
