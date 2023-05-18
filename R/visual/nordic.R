7d971acf02bf1bd6d25214565ba24ad39188cd6d
Husted42 <115221407+Husted42@users.noreply.github.com>
2023-05-18 14:43:25 +0200

    ...

--- R/visual/nordic.R
+++ R/visual/nordic.R
@@ -10,7 +10,7 @@ df_original
 
 ##### -- Clean -- #####
 keep <- c("Denmark", "Norway", "Sweden")
-df_clean <- df_original[df_original$"Country.Name" %in% keep, ] #Keep Denmark, Norway and Sweden
+df_clean <- df_original[df_original$"Country.Code" %in% keep, ] #Keep Denmark, Norway and Sweden
 
 reqd = c()
 for (i in 0:9){
@@ -22,6 +22,7 @@ colnames(df_clean) <- c(1:10)
 years <- seq(2012, 2021)
 df_clean <- rbind(years, df_clean) #Add years to top
 df_clean <- data.frame(t(df_clean[-1])) #Transpose
+colnames(df_clean)
 colnames(df_clean) <- c("year", keep) 
 
 
