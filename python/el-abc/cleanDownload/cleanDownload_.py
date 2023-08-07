0cc96d56379561a2a8a13bf8dd4ce3158dd712c4
Husted42 <alx2608@gmail.com>
2023-08-07 11:14:33 +0200

    ...

--- python/el-abc/cleanDownload/cleanDownload_.py
+++ python/el-abc/cleanDownload/cleanDownload_.py
@@ -1,4 +1,4 @@
-import os, sys
+import os
 import shutil
 import schedule
 import time
@@ -15,6 +15,12 @@ Programming  = ['c', 'cgi', 'class', 'cpp', 'cs', 'h', 'java', 'php', 'py', 'sh'
 path_ = 'C:/Users/alx/Documents/GitHub/code/python/el-abc/cleanDownload'
 
 def categorize(inp):
+    ''' 
+    categorize selects designated folder
+
+    :param inp: name of file with type (file.txt)
+    :return: designated folder name
+    '''
     if inp in Pictures: return 'Pictures'
     if inp in Text: return 'Text'
     if inp in MicrosoftDocuments: return 'MicrosoftDocuments'
@@ -24,33 +30,63 @@ def categorize(inp):
     if inp in Programming: return 'Programming'
     return 'Other'
 
+def duplicateFixer(elm, dest, n):
+    '''
+    duplicateFixer changes filename to filename(n)
+
+    :param elm: name of file (file.txt)
+    :param dest:path to designated folder
+    :param n:   default 0, incremt by one each time function is called 
+    :return: New file name
+    '''
+    elmInDest = os.listdir(dest)
+    if elm in elmInDest:
+        name, type = os.path.splitext(elm)
+        name = name + '({})'.format(n)
+        file = name + type
+        if file in elmInDest:
+            return duplicateFixer(elm, dest, n+1)
+        else: 
+            return file
+    else: 
+        print('return elm')
+        return elm
+
+
 def cleanFolder():
+    ''' 
+    cleanFolder is the main function that iterates through every element in directory, 
+    and move them to designated folder.
+    
+    elm = name of file with type sepecification (file.txt)
+    elm_ = File after duplicate fix (file(n).txt)
+    foldername = Name of the folder the file should be placed in 
+    :returns: None
+    '''
     fileTypes = [] #List of all distinct file types in dir 
     files = os.listdir(path_) #List of all files in dir
     files.remove('cleanDownload.py')
     files.remove('cleanDownload_.py')
-    print(1, files)
     for elm in files:
-        print('Element in question: ', elm, '\n\n\n')
         foldername = ''
-        if (os.path.splitext(elm))[1] != '': #Check if folder
-            print('(after if) elm = ', elm, os.path.splitext(elm))
+        if (os.path.splitext(elm))[1] != '': #Check if element is a folder
             fileSplit = os.path.splitext(elm)
             filetype = (fileSplit[1])[1:] #Filetype minus .
             if fileSplit[1] not in fileTypes: fileTypes.append(fileSplit[1]) #Get distinct
-            print(2, fileSplit[1])
             foldername = categorize(filetype)
         else:
             if elm not in folderLst:
-                print(3, elm) 
                 foldername = 'Folder'
         if foldername != '':
-            print(4, 'foldername = ', foldername)
             src = path_ + '/' + elm
             dest = path_ + '/' + foldername 
             if foldername not in os.listdir(path_): os.mkdir(dest, 0o666)
-            # if os.path.exists(dest):
-            #     shutil.rmtree(dest)
+            elmInDest = os.listdir(dest)
+            #If file already exists
+            if elm in elmInDest:
+                elm_ = duplicateFixer(elm, dest, 0)
+                os.rename(elm, elm_)
+                src = path_ + '/' + elm_
             shutil.move(src, dest)
 
 cleanFolder() #Run startup
