1e685caaf115763b40a1fdb6ce1e04a17168d4e6
Husted42 <alx2608@gmail.com>
2025-07-11 22:40:52 +0200

    ...

--- R/chickens.R
+++ R/chickens.R
@@ -1,2 +1,23 @@
-data3 <- read.delim('~/home/husted42/Downloads/chickenData.txt',header = F)
-head(data3, 5)
+library(dplyr)
+
+# data <- read.delim('~/home/husted42/Downloads/chickenData.txt',header = F)
+# Load dat into table
+data <- read.delim(
+  'C:/Users/Pia/Downloads/chikenData.csv', 
+  sep = ",",
+  header = T)
+head(data, 5)
+
+# Remove non-numeric variables
+data$hens <- as.numeric(data$hens)
+
+data <- data %>% 
+  drop_na()
+
+
+data$Gender
+data$hens
+
+aggregate(data$hens, list(data$Gender), FUN=mean) 
+
+
