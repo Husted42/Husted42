656a2d67281a8f11fea68ca5aea92b63a3d9121e
Husted42 <alx2608@gmail.com>
2025-07-12 12:17:32 +0200

    Added tidyverse :D

--- R/chickens.R
+++ R/chickens.R
@@ -1,7 +1,9 @@
 library(dplyr)
+library(tidyverse)
 
+
+# Load data into table
 # data <- read.delim('~/home/husted42/Downloads/chickenData.txt',header = F)
-# Load dat into table
 data <- read.delim(
   'C:/Users/Pia/Downloads/chikenData.csv', 
   sep = ",",
@@ -10,14 +12,24 @@ head(data, 5)
 
 # Remove non-numeric variables
 data$hens <- as.numeric(data$hens)
+data <- na.omit(data)
 
-data <- data %>% 
-  drop_na()
+# Clean strings
+data$Gender <- str_trim(data$Gender)
 
+# TODO : Outlier filtration
 
+# View columns
+data
 data$Gender
 data$hens
 
 aggregate(data$hens, list(data$Gender), FUN=mean) 
+aggregate(data$hens, list(data$alkohol), FUN=mean) 
+
+
+# TODO : Boxplot
+
+
 
 
